03653b89472e3d9fc4c2a7c447b6f7b0b93a1e77
Connie <conniegiann@gmail.com>
2015-05-30 15:51:02 +1000

    fixed stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,14 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new user_params
+    redirect_to users_path
 
+  end
+
+private
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 
 end
