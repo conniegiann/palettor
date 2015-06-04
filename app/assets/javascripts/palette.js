f68d682ad75e99f71e86b441d7c830d2ac631646
Connie <conniegiann@gmail.com>
2015-06-05 00:47:58 +1000

    landing pg

--- app/assets/javascripts/palette.js
+++ app/assets/javascripts/palette.js
@@ -1,3 +1,4 @@
+// Random Color change for swatches.
 $(document).ready(function (){
   $('form .swatch').click(function(){
     var randomColor = Math.floor(Math.random()*16777215).toString(16);
@@ -5,4 +6,13 @@ $(document).ready(function (){
     $(this).find('input').val('#'+randomColor);
     $(this).find('span').text('#'+randomColor);
   });
+});
+
+
+// Random Color change for dinosaur + colors change on interval timer.
+$(document).ready(function (){
+  window.setInterval(function() {
+    var randomColor = Math.floor(Math.random()*16777215).toString(16);
+      $('path').css('fill', '#'+randomColor);
+  }, 600);
 });
