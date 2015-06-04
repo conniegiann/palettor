d91e91c83cb64d7c6ab38894c3111ca270fea244
Connie <conniegiann@gmail.com>
2015-06-04 12:14:57 +1000

    social, sign in, start of profile page

--- app/assets/javascripts/palette.js
+++ app/assets/javascripts/palette.js
@@ -1,5 +1,5 @@
 $(document).ready(function (){
-  $('.swatch').click(function(){
+  $('form .swatch').click(function(){
     var randomColor = Math.floor(Math.random()*16777215).toString(16);
     $(this).css('background-color', '#'+randomColor);
     $(this).find('input').val('#'+randomColor);
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,16 @@ class UsersController < ApplicationController
     end
   end
     
+  def show
+    set_user
+  end
 
 private
+
+  def set_user
+    @user = User.find params[:id]
+  end
+
   def user_params
     params.require(:user).permit(:name, :password, :password_confirmation, :email)
   end
